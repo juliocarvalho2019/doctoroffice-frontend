621ee9d1bcec0a14feccd45a72dc4a2881779030
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-08-10 13:47:49 -0300

    Feat: feito html e css para medico-list

--- src/app/components/medico/medico-list/medico-list.component.ts
+++ src/app/components/medico/medico-list/medico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Medico } from 'src/app/models/medico';
 
 @Component({
   selector: 'app-medico-list',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicoListComponent implements OnInit {
 
+  ELEMENT_DATA: Medico[] = [
+    {
+      id: 1,
+      nome: 'Valdir Cezar',
+      cpf: '123.456.789-10',
+      email: 'Valdir@email.com',
+      senha: '1234',
+      perfis: ['0'],
+      dataCriacao: '15/08/2022', 
+
+      crm: '"CRM/SP 123452"',
+      especialidade:'Obstetra',
+      idade:'40'
+    }
+  ]
+
+  displayedColumns: string[] = ['id', 'nome', 'crm', 'especialidade','idade','acoes'];
+  dataSource = new MatTableDataSource<Medico>(this.ELEMENT_DATA);
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
---
+++ src/app/models/medico.ts
@@ -0,0 +1,13 @@
+export interface Medico{
+    id?: any;
+    nome: string;
+    cpf: string;
+    email: string;
+    senha: string;
+    perfis: string[];
+    dataCriacao: any;
+
+    crm: string;
+    especialidade: string;
+    idade: any;
+}
