321e97262e71f962204ccc26cf46c787b5e2411b
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-08-10 12:27:36 -0300

    Feat: adicionado rota para componente medico list.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { MedicoListComponent } from './components/medico/medico-list/medico-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
@@ -6,7 +7,8 @@ import { NavComponent } from './components/nav/nav.component';
 const routes: Routes = [
   {
     path: '', component: NavComponent, children: [
-      { path: 'home', component: HomeComponent }
+      { path: 'home', component: HomeComponent },
+      { path: 'medicos', component: MedicoListComponent }
     ]
   }
 ];
