2596c39f35324f6c1c71b6ddc7e7ca19e2b2e0b3
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-08-10 13:54:09 -0300

    Feat: adcionado rota para componente login.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './components/login/login/login.component';
 import { MedicoListComponent } from './components/medico/medico-list/medico-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -5,6 +6,7 @@ import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 
 const routes: Routes = [
+  {path: 'login', component: LoginComponent},
   {
     path: '', component: NavComponent, children: [
       { path: 'home', component: HomeComponent },
