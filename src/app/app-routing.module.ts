e00a886b3d7603500795318be618ac41aab375ae
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-08-10 15:19:24 -0300

    Feat: autenticacao login.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { LoginComponent } from './components/login/login/login.component';
 import { MedicoListComponent } from './components/medico/medico-list/medico-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {
-    path: '', component: NavComponent, children: [
+    path: '', component: NavComponent, canActivate: [AuthGuard], children: [
       { path: 'home', component: HomeComponent },
       { path: 'medicos', component: MedicoListComponent }
     ]
