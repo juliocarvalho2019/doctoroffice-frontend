849910973165bf09907931a8187d0997f9f357ff
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-08-11 08:27:52 -0300

    Feat: criado rota para componente tecnico create.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,20 @@
+
 import { LoginComponent } from './components/login/login/login.component';
-import { MedicoListComponent } from './components/medico/medico-list/medico-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
+import { MedicoCreateComponent } from './components/medico/medico-create/medico-create.component';
+import { MedicoListComponent } from './components/medico/medico-list/medico-list.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {
     path: '', component: NavComponent, canActivate: [AuthGuard], children: [
       { path: 'home', component: HomeComponent },
-      { path: 'medicos', component: MedicoListComponent }
+      { path: 'medicos', component: MedicoListComponent },
+      { path: 'medicos/create', component: MedicoCreateComponent }
     ]
   }
 ];
