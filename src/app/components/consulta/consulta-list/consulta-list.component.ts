cfcd1de2eab715a58dec3c3c5f1de336a249a6b0
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-08-12 08:48:34 -0300

    Feat: filtro e metodo findall para consulta.

--- src/app/components/consulta/consulta-list/consulta-list.component.ts
+++ src/app/components/consulta/consulta-list/consulta-list.component.ts
@@ -1,3 +1,4 @@
+import { ConsultaService } from './../../../services/consulta.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
@@ -10,33 +11,64 @@ import { Consulta } from 'src/app/models/consulta';
 })
 export class ConsultaListComponent implements OnInit {
 
-  ELEMENT_DATA: Consulta[] = [
-    {
-      id:                          1,
-      dataAbertura:     '21/06/2021',
-      dataFechamento:   '21/06/2021',
-      prioridade:             'ALTA',
-      status:            'ANDAMENTO',
-      medico:            	  	   1,
-      paciente:                     6,
-      nomePaciente: '   Valdir Cezar',
-      nomeMedico: 'Albert Einstein',
-    }
-  ]
+  ELEMENT_DATA: Consulta[] = []
+  FILTERED_DATA: Consulta[] = []
 
   displayedColumns: string[] = ['id', 'paciente', 'medico', 'dataAbertura', 'prioridade', 'status', 'acoes'];
   dataSource = new MatTableDataSource<Consulta>(this.ELEMENT_DATA);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor() { }
+  constructor(
+    private service: ConsultaService
+  ) { }
 
   ngOnInit(): void {
+    this.findAll();
+  }
+
+  findAll(): void {
+    this.service.findAll().subscribe(resposta => {
+      this.ELEMENT_DATA = resposta;
+      this.dataSource = new MatTableDataSource<Consulta>(resposta);
+      this.dataSource.paginator = this.paginator;
+    })
   }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  retornaStatus(status: any): string {
+    if(status == '0') {
+      return 'ABERTO'
+    } else if(status == '1') {
+      return 'EM ANDAMENTO'
+    } else {
+      return 'ENCERRADO'
+    }
+  }
+
+  retornaPrioridade(prioridade: any): string {
+    if(prioridade == '0') {
+      return 'BAIXA'
+    } else if(prioridade == '1') {
+      return 'MÉDIA'
+    } else {
+      return 'ALTA'
+    }
+  }
+
+  orderByStatus(status: any): void{
+    let list: Consulta[] = []
+    this.ELEMENT_DATA.forEach(element => {
+      if(element.status == status)
+        list.push(element)
+    });
+    this.FILTERED_DATA = list;
+    this.dataSource = new MatTableDataSource<Consulta>(list);
+    this.dataSource.paginator = this.paginator;
   }
 
 }
---
+++ src/app/services/consulta.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { API_CONFIG } from "../config/api.config";
+import { Consulta } from "../models/consulta";
+
+@Injectable({
+    providedIn: 'root'
+  })
+  export class ConsultaService {
+  
+    constructor(private http: HttpClient) { }
+  
+    findAll(): Observable<Consulta[]> {
+      return this.http.get<Consulta[]>(`${API_CONFIG.baseUrl}/consultas`);
+    }
+  }
