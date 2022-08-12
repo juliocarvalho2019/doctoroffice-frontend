0400230eb91d4aaf5cabb7afd1f7b14169c328c8
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2022-08-12 09:10:22 -0300

    Feat: validação de formulários para consulta-create.

--- src/app/components/consulta/consulta-create/consulta-create.component.ts
+++ src/app/components/consulta/consulta-create/consulta-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-consulta-create',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsultaCreateComponent implements OnInit {
 
+  prioridade: FormControl = new FormControl(null, [Validators.required])
+  status: FormControl = new FormControl(null, [Validators.required])
+  medico: FormControl = new FormControl(null, [Validators.required])
+  paciente: FormControl = new FormControl(null, [Validators.required])
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  validaCampos(): boolean {
+    return this.prioridade.valid && this.status.valid &&
+      this.medico.valid && this.paciente.valid
   }
 
 }
